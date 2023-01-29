71fc6a36ac1c2c3aff4fce8f8c335ff08fc6cc0f
Johnathan Booy <johnny_music@icloud.com>
2023-01-29 14:01:37 -0800

    Removed unused constructor function

--- src/Joke.js
+++ src/Joke.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./Joke.css";
 
 class Joke extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		const { vote, votes, text, id } = this.props;
 		return (
