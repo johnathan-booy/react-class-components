6db9b830a1ed0a2ddbb65ff9515246657449487d
Johnathan Booy <johnny_music@icloud.com>
2023-01-29 12:48:47 -0800

    Refactor Joke to class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import JokeList from "./JokeList";
 
 function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+	return (
+		<div className="App">
+			<JokeList />
+		</div>
+	);
 }
 
 export default App;
--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,31 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+	render() {
+		const { vote, votes, text, id } = this.props;
+		return (
+			<div className="Joke">
+				<div className="Joke-votearea">
+					<button onClick={() => vote(id, +1)}>
+						<i className="fas fa-thumbs-up" />
+					</button>
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+					<button onClick={() => vote(id, -1)}>
+						<i className="fas fa-thumbs-down" />
+					</button>
 
-        {votes}
-      </div>
+					{votes}
+				</div>
 
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+				<div className="Joke-text">{text}</div>
+			</div>
+		);
+	}
 }
 
 export default Joke;
